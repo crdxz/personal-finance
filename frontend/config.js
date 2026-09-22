596be81cf50eecd1bbf8511f53bb45e64a2fb3a2
Crdxz <yeisson.cardozo222@gmail>
2026-09-22 13:05:27 -0500

    Connect frontend to deployed services

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_API_URL: "http://127.0.0.1:8000/api/v1",
-  FINANCE_API_URL: "http://127.0.0.1:8001/api/v1",
-  REPORT_API_URL: "http://127.0.0.1:8002/api/v1",
+  AUTH_API_URL: "https://finance-auth-virid.vercel.app/api/v1",
+  FINANCE_API_URL: "https://finance-service-six.vercel.app/api/v1",
+  REPORT_API_URL: "https://finance-report-seven.vercel.app/api/v1",
 };
