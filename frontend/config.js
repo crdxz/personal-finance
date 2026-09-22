4645e50a66495b04f04788ccf16ebe33151a44d5
Crdxz <yeisson.cardozo222@gmail>
2026-09-22 15:21:23 -0500

    Update deployed report service URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   AUTH_API_URL: "https://finance-auth-virid.vercel.app/api/v1",
   FINANCE_API_URL: "https://finance-service-six.vercel.app/api/v1",
-  REPORT_API_URL: "https://finance-report-seven.vercel.app/api/v1",
+  REPORT_API_URL: "https://report-service-psi.vercel.app/api/v1",
 };
